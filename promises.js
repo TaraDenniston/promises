922b23ef5c6b673d67be7783b096b75189a6eacc
Tara Denniston <taradenniston@gmail.com>
2023-08-25 22:15:42 -0400

    complete exercise 1.3

--- promises.js
+++ promises.js
@@ -15,11 +15,22 @@ let numList = [7,11,13,17];
 
 axios.get(`${baseUrl}/${numList}?json`)
   .then(response => {
+    $('body').append('<p>2.)</p><ul id="two"></ul>')
     for (let item of numList) {
-      console.log(response.data[item])
+      $('#two').append(`<li>${response.data[item]}</li>`);
     }
-    return response.data
   })
   .catch(error => console.log(error));
 
 
+// 3.
+let promises = Array.from({ length: 4 }, () => {
+    return axios.get(`${baseUrl}/${favNum}?json`);
+  });
+
+Promise.all(promises)
+  .then(responseArr => {
+    $('body').append('<p>3.)</p><ul id="three"></ul>')
+    responseArr.forEach(r => $('#three').append(`<li>${r.data.text}</li>`))
+  })
+  .catch(error => console.log(error));
