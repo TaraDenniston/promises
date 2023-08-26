274b9e2515d04ad661640a627a50c2c31686478c
Tara Denniston <taradenniston@gmail.com>
2023-08-25 21:24:36 -0400

    complete exercise 1.2

--- promises.js
+++ promises.js
@@ -1,8 +1,25 @@
 // Part 1: Number Facts
+let baseUrl = 'http://numbersapi.com';
+
 
 // 1.
 let favNum = 19;
 
-$.getJSON(`http://numbersapi.com/${favNum}?json`, response => {
-    console.log(response)
-})
+axios.get(`${baseUrl}/${favNum}?json`)
+  .then(response => {console.log(response.data);})
+  .catch(error => console.log(error));
+
+
+// 2.
+let numList = [7,11,13,17];
+
+axios.get(`${baseUrl}/${numList}?json`)
+  .then(response => {
+    for (let item of numList) {
+      console.log(response.data[item])
+    }
+    return response.data
+  })
+  .catch(error => console.log(error));
+
+
